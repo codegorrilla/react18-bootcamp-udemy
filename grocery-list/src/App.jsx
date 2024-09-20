57db4434ad96c8c8b2f6f4bfcdf29c756aadf1ca
Sanjib Adhya <sanjib@zacksresearch.in>
2024-09-20 09:07:47 -0500

    added function for checking items

--- grocery-list/src/App.jsx
+++ grocery-list/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import groceryCart from './assets/grocery-cart.png';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // {
 //   name: 'Banana',
@@ -13,15 +13,23 @@ function App() {
 
 	const [groceryItems, setGroceryItems] = useState([]);
 
+	const [isCompleted, setIsCompleted] = useState(false);
+
+	useEffect(()=>{
+		console.log('Called');
+	}, [groceryItems])
+
+	//getting the input value
 	const handleChangeInputValue = (e) => {
 		setInputVal(e.target.value);
 	};
 
+	//adding input values as list items
 	const handleAddGroceryItem = (e) => {
 		if (e.key === 'Enter') {
 			if (inputVal) {
 				const updatedGroceryList = [...groceryItems];
-        //console.log(updatedGroceryList);
+				//console.log(updatedGroceryList);
 				const itemIndex = updatedGroceryList.findIndex(
 					(item) => item.name === inputVal
 				);
@@ -32,44 +40,100 @@ function App() {
 						quantity: 1,
 						completed: false,
 					});
-				}else{
-          updatedGroceryList[itemIndex].quantity++
-        }
+				} else {
+					updatedGroceryList[itemIndex].quantity++;
+				}
 
-        setGroceryItems(updatedGroceryList);
+				setGroceryItems(updatedGroceryList);
 				setInputVal('');
 			}
 		}
 	};
 
-  const handleRemoveItem = (name)=>{
-    //console.log(name);
-    const updatedGroceryList = [...groceryItems].filter(item => item.name !== name);
+	console.log(groceryItems);
 
-    setGroceryItems(updatedGroceryList);
-  }
+	//deleting an item
+	const handleRemoveItem = (name) => {
+		//console.log(name);
+		const updatedGroceryList = [...groceryItems].filter(
+			(item) => item.name !== name
+		);
 
+		setGroceryItems(updatedGroceryList);
+	};
+
+	//mark an item as done
+	const handleUpdateCompleteStatus = (status, index) => {
+		const updatedGroceryList = [...groceryItems];
+
+		updatedGroceryList[index].completed = status;
+
+		setGroceryItems(updatedGroceryList);
+	};
+
+	// const determineCompletedStatus = ()=>{
+	// 	if(!groceryItems.length){
+	// 		return setIsCompleted(false)
+	// 	}
+
+	// 	let isAllCompleted = true;
+
+	// 	groceryItems.forEach(item=>{
+	// 		if(!item.completed) isAllCompleted = false;
+	// 	});
+
+	// 	setIsCompleted(isAllCompleted);
+	// }
+
+	//rendering item list
 	const renderGroceryList = () => {
-		return groceryItems.map((item) => (
+		return groceryItems.map((item, index) => (
 			<li key={item.name}>
 				<div className="container">
-					<input type="checkbox" />
-					<p>{item.name}{' '}{item.quantity > 1 && <span>x{item.quantity}</span>}</p>
+					<input
+						type="checkbox"
+						value={item.completed}
+						checked={item.completed}
+						onChange={(e) => {
+							console.log(e.target.checked);
+							handleUpdateCompleteStatus(e.target.checked, index);
+						}}
+					/>
+					<p>
+						{item.name} {item.quantity > 1 && <span>x{item.quantity}</span>}
+					</p>
 				</div>
 				<div>
-					<button className="remove-button" onClick={()=> handleRemoveItem(item.name)}>x</button>
+					<button
+						className="remove-button"
+						onClick={() => handleRemoveItem(item.name)}
+					>
+						x
+					</button>
 				</div>
 			</li>
 		));
 	};
 
-  
-
 	return (
 		<main className="App">
 			<div>
 				<div>
-					<h4 className="success">You&#39;re done</h4>
+					{/* <button
+						onClick={() => {
+							const updatedGroceryList = [...groceryItems].map((item) => {
+								return {
+									...item,
+									completed: false,
+								};
+							});						
+
+							setGroceryItems(updatedGroceryList);
+						}}
+					>
+						Clear all checks
+					</button> */}
+					{isCompleted && <h4 className="success">You&#39;re done</h4>}
 					{/* {JSON.stringify(groceryItems)} */}
 					<div className="header">
 						<h1>Shopping List</h1>
