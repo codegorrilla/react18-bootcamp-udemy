592a83e6aa10520fe0f8c3f06c3e62b43d943d25
Sanjib Adhya <code.gorrilla@gmail.com>
2024-11-28 07:30:19 +0530

    added fetching data via an API for generating card items

--- recipe-app/src/pages/HomePage.jsx
+++ recipe-app/src/pages/HomePage.jsx
@@ -1,11 +1,44 @@
 import CardList from '../components/CardList';
 import Header from '../components/Header';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const options = {
+  method: 'GET',
+  url: 'https://tasty.p.rapidapi.com/recipes/list',
+  params: {
+    from: '0',
+    size: '20',
+  },
+  headers: {
+    'x-rapidapi-key': 'd666749ac3mshb244efd8aec2605p10b5fajsnf83843d6628e',
+    'x-rapidapi-host': 'tasty.p.rapidapi.com'
+  }
+};
+
 
 export default function HomePage() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(()=>{
+    fetchRecipes();
+  }, [])
+
+  const fetchRecipes = async()=>{
+    try {
+      const response = await axios.request(options);
+
+      console.log(response.data)
+      setRecipes(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <>
       <Header />
-      <CardList />
+      <CardList recipes={recipes}/>
     </>
   );
 }
--- recipe-app/src/pages/RecipePage.jsx
+++ recipe-app/src/pages/RecipePage.jsx
@@ -1,17 +1,17 @@
 import { useParams } from 'react-router-dom';
-import { recipes } from '../components/CardList';
+//import { recipes } from '../components/CardList';
 
 export default function RecipePage() {
   const { id } = useParams();
   //console.log(typeof id);
 
-  const recipe = recipes.find((r) => r.id === parseInt(id));
+  //const recipe = recipes.find((r) => r.id === parseInt(id));
 
   //console.log(recipe)
 
   return (
     <div>
-      <h1>{recipe.name}</h1>
+      {/* <h1>{recipe.name}</h1> */}
     </div>
   );
 }
